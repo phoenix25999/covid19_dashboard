b09e67e1b0449e1f17af0af8f1b258889d3f2b39
VisheshTTN <vishesh.srivastava@tothenew.com>
2020-04-25 18:51:37 +0530

    Updates

---
+++ precache-manifest.77df440704156e8b2ee7fd612cf5c416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ea6c7525faaae1bf11be9f2b3c8ec0",
+    "url": "/covid19_dashboard/index.html"
+  },
+  {
+    "revision": "d870b807cadc0f507ceb",
+    "url": "/covid19_dashboard/static/css/2.72d5056d.chunk.css"
+  },
+  {
+    "revision": "92090a53d2b89340f991",
+    "url": "/covid19_dashboard/static/css/main.eb9449fc.chunk.css"
+  },
+  {
+    "revision": "d870b807cadc0f507ceb",
+    "url": "/covid19_dashboard/static/js/2.36e24b62.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/covid19_dashboard/static/js/2.36e24b62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92090a53d2b89340f991",
+    "url": "/covid19_dashboard/static/js/main.d65f87e4.chunk.js"
+  },
+  {
+    "revision": "2fdbdac2919742909e8d",
+    "url": "/covid19_dashboard/static/js/runtime-main.847fbda5.js"
+  }
+]);
